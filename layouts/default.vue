1600b9c947084b98f0581bb856035ecac57e892b
CHamburr <hi@chamburr.xyz>
2021-03-14 16:56:04 +0800

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex flex-column vh-100">
+    <div class="d-flex flex-wrap bg-dark rounded align-items-center mx-4 mx-lg-5 mt-4 mb-2 px-4 px-lg-5 py-2">
+      <div class="d-flex align-items-center mr-4">
+        <img width="50px" class="py-1" src="/icon.png" alt="CHamburr">
+        <p class="mb-0 mx-3 font-weight-bold text-white">CHamburr</p>
+      </div>
+      <div class="d-flex">
+        <a href="/" class="nav-link pl-0 pr-3">Home</a>
+        <a href="/blog" target="_blank" class="nav-link px-3">Blog</a>
+        <a href="mailto:hi@chamburr.xyz" target="_blank" class="nav-link pl-3 pr-0">Contact</a>
+      </div>
+    </div>
+    <nuxt class="flex-grow-1" />
+    <footer class="h5 text-center pt-2 pb-4 pb-lg-5">
+      Copyright © CHamburr 2021
+    </footer>
+  </div>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,82 @@
+export default {
+  target: 'static',
+
+  head: {
+    htmlAttrs: {
+      lang: 'en',
+    },
+    title: 'CHamburr',
+    meta: [
+      { hid: 'charset', charset: 'utf-8' },
+      { hid: 'viewport', name: 'viewport', content: 'initial-scale=1.0' },
+      { hid: 'description', name: 'description', content: 'CHamburr\'s personal website.' },
+      { hid: 'theme-color', name: 'theme-color', content: '#1e90ff' },
+      { hid: 'og:title', property: 'og:title', content: 'CHamburr' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://chamburr.xyz' },
+      { hid: 'og:image', property: 'og:image', content: 'https://chamburr.xyz/icon.png' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'CHamburr' },
+      { hid: 'og:description', property: 'og:description', content: 'CHamburr\'s personal website.' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'CHamburr' },
+      { hid: 'twitter:image', name: 'twitter:image', content: 'https://chamburr.xyz/icon.png' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@CHamburr22' },
+      { hid: 'twitter:description', name: 'twitter:description', content: 'CHamburr\'s personal website.' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#1e90ff' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'canonical', href: 'https://chamburr.xyz' },
+    ],
+  },
+
+  css: ['~/assets/css/styles.css'],
+
+  generate: {
+    crawler: false,
+  },
+
+  loading: {
+    color: '#1e90ff',
+    height: '2px',
+  },
+
+  buildModules: [
+    '@nuxtjs/fontawesome',
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/pwa',
+    'bootstrap-vue/nuxt',
+    'nuxt-purgecss',
+  ],
+
+  bootstrapVue: {
+    directives: ['VBTooltip'],
+  },
+
+  fontawesome: {
+    component: 'fa',
+    icons: {
+      solid: [
+        'faArrowLeft',
+        'faAt',
+      ],
+      brands: [
+        'faDiscord',
+        'faFacebook',
+        'faGithub',
+        'faInstagram',
+        'faKeybase',
+        'faLinkedin',
+        'faTwitter',
+      ],
+    },
+  },
+
+  googleAnalytics: {
+    id: 'UA-129600306-6',
+  },
+}
