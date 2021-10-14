edfe00235a182f7ff5df081b390485b0a6433105
Han Cen <hi@chamburr.com>
2021-10-14 15:21:30 +0800

    Update email address

--- pages/index.vue
+++ pages/index.vue
@@ -64,7 +64,7 @@ export default {
         },
         {
           "name": "Email",
-          "url": "mailto:hi@chamburr.xyz",
+          "url": "mailto:hi@chamburr.com",
           "icon": ["fas", "at"],
         },
         {
