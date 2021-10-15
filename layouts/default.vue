82fddffe06a4062077870b0eba18a2dd76bff1b2
Yikun Zhou <34623167+PorridgePi@users.noreply.github.com>
2021-10-15 17:23:40 +0800

    Fix viewport height (#7)
    To fix site displaying incorrectly on iOS Safari (versions eariler than Safari 15).

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-column vh-100">
+  <div class="d-flex flex-column min-vh-100">
     <div class="d-flex flex-wrap bg-dark rounded align-items-center mx-4 mx-lg-5 mt-4 mb-2 px-4 px-lg-5 py-2">
       <div class="d-flex align-items-center mr-4">
         <img width="50px" class="py-1" src="/icon.png" alt="CHamburr">
